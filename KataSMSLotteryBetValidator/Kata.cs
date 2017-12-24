b444e4262d30eee9005cdf0fb0bcdb98c1e48e3a
Js-Yang <chyang590@gmail.com>
2017-12-24 23:02:41 +0800

    add test case ValidateBet_Count_Is_1_Max_Is_1_numbers_are_1_Should_Return_1

--- KataSMSLotteryBetValidator/Kata.cs
+++ KataSMSLotteryBetValidator/Kata.cs
@@ -1,6 +1,14 @@
-﻿namespace KataSMSLotteryBetValidator
+﻿using System;
+using System.Linq;
+
+namespace KataSMSLotteryBetValidator
 {
     public class Kata
     {
+        public static int[] ValidateBet(int count, int maxNum, string numbers)
+        {
+            var LotteryNumber = numbers.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+            return LotteryNumber;
+        }
     }
 }
--- SMSLotteryBetValidator.Tests/KataTest.cs
+++ SMSLotteryBetValidator.Tests/KataTest.cs
@@ -1,6 +1,17 @@
-﻿namespace SMSLotteryBetValidator.Tests
+﻿using KataSMSLotteryBetValidator;
+using NUnit.Framework;
+
+namespace SMSLotteryBetValidator.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class KataTests
     {
+        [Test]
+        public void ValidateBet_Count_Is_1_Max_Is_1_numbers_are_1_Should_Return_1()
+        {
+            var expected = new int[] { 1 };
+            var result = Kata.ValidateBet(1, 1, "1");
+            Assert.AreEqual(expected, result);
+        }
     }
 }
